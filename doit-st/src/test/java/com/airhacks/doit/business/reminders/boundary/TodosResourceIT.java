db129bb2a5e83e907d0144b71058743d1ce68dd4
Adam Bien <abien@adam-bien.com>
2015-06-15 09:10:11 +0200

    System test works

---
+++ doit-st/src/test/java/com/airhacks/doit/business/reminders/boundary/TodosResourceIT.java
@@ -0,0 +1,30 @@
+package com.airhacks.doit.business.reminders.boundary;
+
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
+import static com.airhacks.rulz.jaxrsclient.JAXRSClientProvider.buildWithURI;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class TodosResourceIT {
+
+    @Rule
+    public JAXRSClientProvider provider = buildWithURI("http://localhost:8080/doit/api/todos");
+
+    @Test
+    public void fetchToDos() {
+        Response response = this.provider.target().request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.startsWith("hey"));
+    }
+
+}
